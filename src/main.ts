4acc3712e2eb1c61ef0731a12219e6b48d221b42
Alan Rawkins <alan@rawkins.com>
2018-07-28 07:36:54 -0700

    Upgraded webpack, added loaders for css, html generation

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,41 @@
 import { Circle } from './circle';
+import { iShape } from './interfaces';
+import './style.css';
 
 var canvas: HTMLCanvasElement;
 var ctx: CanvasRenderingContext2D;
-var circle1: Circle = new Circle(200, 300, 50);
-var circle2: Circle = new Circle(400, 550, 150, "blue", 5);
-var circle3:Circle = new Circle(50,50,10,"red");
-var circles:Array<Circle> = [];
+var shapes:Array<iShape> = [];
 
 function gameLoop() {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1280, 720);
-    
-    circles.forEach(c => {
+
+    shapes.forEach(c => {
         c.move();
         c.draw(ctx);
     });
-    
+
 }
 
 window.onload = () => {
-    canvas = <HTMLCanvasElement>document.getElementById('game');
+    let container = document.createElement('div');
+    container.id = "container";
+
+    canvas = document.createElement('canvas');
+    canvas.id     = "game";
+    canvas.width  = 1280;
+    canvas.height = 720;
+    canvas.classList.add('red_border');
+    container.appendChild(canvas);
+    document.body.appendChild(container);
     ctx = canvas.getContext("2d");
-    for(var i=0; i<120; i++) {
-        var c:Circle = new Circle(Math.random()*1280, Math.random()*720, Math.random()*150, "blue", 3);
-        circles.push(c);
-    }
+    
     gameLoop();
+
+    canvas.onclick = (e) => {
+        console.log(e);
+        var c:Circle = new Circle(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, Math.random()*25, "blue");
+        shapes.push(c);
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/main.ts',
     devtool: 'inline-source-map',
     module: {
-        rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
@@ -19,6 +30,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist/')
     },
     plugins: [
-        new UglifyJSPlugin()
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({title: 'Typescript Project'})
     ]
 };
